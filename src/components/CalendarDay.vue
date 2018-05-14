0c3dfbca0d6f3d94158050be7b3f581fc840bb2c
Daryl Daly <dwdaly@yahoo.com>
2018-05-13 22:05:34 -0700

    course finished

--- src/components/CalendarDay.vue
+++ src/components/CalendarDay.vue
@@ -1,25 +1,36 @@
 <template>
   <div :class="classObject" @click="captureClick">
     {{ day.format('D') }}
+    <ul class="event-list">
+      <li v-for="event in events">{{ event.description }}</li>
+    </ul>
   </div>
 </template>
 <script>
   export default {
     props: [ 'day' ],
     computed: {
+      events() {
+        return this.$store.state.events.filter(event => event.date.isSame(this.day, 'day'))
+      },
       classObject() {
+        let eventFormDate = this.$store.state.eventFormDate
+        let eventFormActive = this.$store.state.eventFormActive
         let today = this.$moment()
         let isToday = this.day.isSame(today, 'day')
         return {
           day: true,
           today: isToday,
-          past: this.day.isSameOrBefore(today, 'day') && ! isToday
+          past: this.day.isSameOrBefore(today, 'day') && ! isToday,
+          active: eventFormDate.isSame(this.day,'day') && eventFormActive
         }
       }
     },
     methods: {
       captureClick(event) {
         this.$store.commit('eventFormPos', { x: event.clientX, y: event.clientY })
+        this.$store.commit('eventFormActive', true)
+        this.$store.commit('eventFormDate', this.day)
       },
     },
   }
